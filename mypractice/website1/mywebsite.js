085a9ad0f125e3981c2aff05bda0d06f34b10646
ymjrcc <ymjrcc@qq.com>
2015-07-19 02:25:50 +0800

    add sth

--- mypractice/website1/mywebsite.js
+++ mypractice/website1/mywebsite.js
@@ -4,6 +4,14 @@ var cnt = 0;
 
 function autoplay(){
 	if(arr.length == cnt) cnt = 0;
+	if(cnt==1||cnt==2||cnt==3){
+		$('#span'+(cnt+1)).addClass('red');
+		$('#span'+cnt).removeClass('red');		
+	}
+	else if(cnt==0){
+		$('#span1').addClass('red');
+		$('#span4').removeClass('red');
+	}
 	document.getElementById('banner1').src = arr[cnt];
 
 	cnt++;
@@ -17,6 +25,12 @@ function clearTimer(){
 
 //鼠标悬停时指定播放的图片
 function showBannerById(num){
+	
+	$('#span1').removeClass('red');
+	$('#span2').removeClass('red');
+	$('#span3').removeClass('red');
+	$('#span4').removeClass('red');
+
 	clearTimer();
 	var index = parseInt(num);
 	document.getElementById('banner1').src = arr[index-1];
