55c6b6a27c1261c7957b9b394b086f0131bfc86d
ymjrcc <ymjrcc@qq.com>
2015-07-19 02:48:04 +0800

    fixed bug

--- mypractice/website1/mywebsite.js
+++ mypractice/website1/mywebsite.js
@@ -1,45 +1,43 @@
-$(document).ready(function(){
-	var arr = ["images/banner1.jpg","images/banner2.jpg","images/banner3.jpg","images/banner4.jpg"];
+var arr = ["images/banner1.jpg","images/banner2.jpg","images/banner3.jpg","images/banner4.jpg"];
 
-	var cnt = 0;
+var cnt = 0;
 
-	function autoplay(){
-		if(arr.length == cnt) cnt = 0;
-		if(cnt==1||cnt==2||cnt==3){
-			$('#span'+(cnt+1)).addClass('red');
-			$('#span'+cnt).removeClass('red');		
-		}
-		else if(cnt==0){
-			$('#span1').addClass('red');
-			$('#span4').removeClass('red');
-		}
-		document.getElementById('banner1').src = arr[cnt];
-
-		cnt++;
+function autoplay(){
+	if(arr.length == cnt) cnt = 0;
+	if(cnt==1||cnt==2||cnt==3){
+		$('#span'+(cnt+1)).addClass('red');
+		$('#span'+cnt).removeClass('red');		
 	}
-
-	var timer = setInterval(autoplay,2000);
-
-	function clearTimer(){
-		clearInterval(timer);
-	}
-
-	//鼠标悬停时指定播放的图片
-	function showBannerById(num){
-		
-		$('#span1').removeClass('red');
-		$('#span2').removeClass('red');
-		$('#span3').removeClass('red');
+	else if(cnt==0){
+		$('#span1').addClass('red');
 		$('#span4').removeClass('red');
-
-		clearTimer();
-		var index = parseInt(num);
-		document.getElementById('banner1').src = arr[index-1];
-		cnt = index;
 	}
+	document.getElementById('banner1').src = arr[cnt];
 
-	//鼠标离开时恢复动画
-	function btnMouseOut(){
-		timer = setInterval(autoplay,2000);
-	}
+	cnt++;
+}
+
+var timer = setInterval(autoplay,2000);
+
+function clearTimer(){
+	clearInterval(timer);
+}
+
+//鼠标悬停时指定播放的图片
+function showBannerById(num){
+	
+	$('#span1').removeClass('red');
+	$('#span2').removeClass('red');
+	$('#span3').removeClass('red');
+	$('#span4').removeClass('red');
+
+	clearTimer();
+	var index = parseInt(num);
+	document.getElementById('banner1').src = arr[index-1];
+	cnt = index;
+}
+
+//鼠标离开时恢复动画
+function btnMouseOut(){
+	timer = setInterval(autoplay,2000);
 }
